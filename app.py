90c524e7737bd51d63c6956ce62640edaf19c373
oscar novoa <novthel@live.com>
2022-03-03 13:53:37 -0500

    segundo commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sqlite3
 
 def cargarArchivo():
 
-    a= open('medals_total.csv', mode='r')
+    a= open('medals_total.csv', mode='r', encoding='utf-8-sig')
     next(a,None)
     lectura = csv.reader(a, delimiter=';')
     return lectura
@@ -32,11 +32,30 @@ def guardarArchivo(archivo):
     except sqlite3.OperationalError as error:
 	    print("Error al abrir:", error)
 
-    
+
+def descargar():
+    try:
+        with sqlite3.connect('BD.db') as bd:
+            cursor = bd.cursor()
+            cursor.execute("SELECT * FROM BD")
+            data = cursor.fetchall()
+
+            encabezado= ['PAIS','ORO','PLATA','BRONCE','TOTAL','PUESTO']
+            nuevo= open('nuevoArchivo.csv' , mode='w', encoding='utf-8-sig')
+            escritura = csv.writer(nuevo, delimiter=';')
+
+            for fila in data:
+                escritura.writerow(fila)
+
+    except sqlite3.OperationalError as error:
+	    print("Error al abrir:", error)
+
+
 
 if __name__ == '__main__':
 
     archivo = cargarArchivo()
-
     guardarArchivo(archivo)
+
+    descargar()
 
