5340dcca624fa115396fe5f1f1f980c9a436b17b
oscar novoa <novthel@live.com>
2022-03-04 18:59:35 -0500

    modifica a OOP

--- app.py
+++ app.py
@@ -2,60 +2,66 @@
 import csv
 import sqlite3
 
-def cargarArchivo():
 
-    a= open('medals_total.csv', mode='r', encoding='utf-8-sig')
-    next(a,None)
-    lectura = csv.reader(a, delimiter=';')
-    return lectura
-        
+class ArchivoCSV():
+    pass
 
-def guardarArchivo(archivo):
-    try:
-        with sqlite3.connect('BD.db') as bd:
+    def cargar(self):
+
+        a= open('medals_total.csv', mode='r', encoding='utf-8-sig')
+        next(a,None)
+        lectura = csv.reader(a, delimiter=';')
+        return self.guardar(lectura)
             
-            cursor = bd.cursor()
-            cursor.execute("""CREATE TABLE IF NOT EXISTS BD(
-                                        PAIS TEXT NOT NULL,
-                                        ORO INTEGER NOT NULL,
-                                        PLATA INTEGER NOT NULL,
-                                        BRONCE INTEGER NOT NULL,
-                                        TOTAL INTEGER NOT NULL,
-                                        PUESTO INTEGER NOT NULL
-                                                            ) """)
+
+    def guardar(self,archivo):
+        try:
+            with sqlite3.connect('BD.db') as bd:
+                
+                cursor = bd.cursor()
+                cursor.execute("""CREATE TABLE IF NOT EXISTS BD(
+                                            PAIS TEXT NOT NULL,
+                                            ORO INTEGER NOT NULL,
+                                            PLATA INTEGER NOT NULL,
+                                            BRONCE INTEGER NOT NULL,
+                                            TOTAL INTEGER NOT NULL,
+                                            PUESTO INTEGER NOT NULL
+                                                                ) """)
+                
+                for row in archivo:
+                    cursor.execute("INSERT INTO BD VALUES (?,?,?,?,?,?)",(row[1],row[2],row[3],row[4],row[5],row[6]))
+                bd.commit()
+                print('Archivo guardado correctamente')
+
+                return self.descargar()
             
-            for row in archivo:
-                cursor.execute("INSERT INTO BD VALUES (?,?,?,?,?,?)",(row[1],row[2],row[3],row[4],row[5],row[6]))
-            bd.commit()
-            print('Archivo guardado correctamente')
-        
-    except sqlite3.OperationalError as error:
-	    print("Error al abrir:", error)
+        except sqlite3.OperationalError as error:
+            print("Error al abrir:", error)
 
 
-def descargar():
-    try:
-        with sqlite3.connect('BD.db') as bd:
-            cursor = bd.cursor()
-            cursor.execute("SELECT * FROM BD")
-            data = cursor.fetchall()
+    def descargar(self):
+        try:
+            with sqlite3.connect('BD.db') as bd:
+                cursor = bd.cursor()
+                cursor.execute("SELECT * FROM BD")
+                data = cursor.fetchall()
 
-            encabezado= ['PAIS','ORO','PLATA','BRONCE','TOTAL','PUESTO']
-            nuevo= open('nuevoArchivo.csv' , mode='w', encoding='utf-8-sig')
-            escritura = csv.writer(nuevo, delimiter=';')
+                nuevo= open('nuevoArchivo.csv' , mode='w', encoding='utf-8-sig')
+                escritura = csv.writer(nuevo, delimiter=';')
 
-            for fila in data:
-                escritura.writerow(fila)
+                for fila in data:
+                    escritura.writerow(fila)
+                
+                print('Archivo creado correctamente')
 
-    except sqlite3.OperationalError as error:
-	    print("Error al abrir:", error)
+        except sqlite3.OperationalError as error:
+            print("Error al abrir:", error)
 
 
 
 if __name__ == '__main__':
 
-    archivo = cargarArchivo()
-    guardarArchivo(archivo)
-
-    descargar()
+    a = ArchivoCSV()
+    
+    lectura = a.cargar()
 
